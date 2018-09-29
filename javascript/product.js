a180630c6d43aa4714c187c2d5e26ec3ccae0159
Gavin Tang <gavintang13@gmail.com>
2018-09-29 17:25:59 -0500

    Removed custom order, added flexbox, modal viewing, placeholders.

--- javascript/product.js
+++ javascript/product.js
@@ -22,6 +22,25 @@ function check(){
   }
 }
 
+//Image viewer
+var close = document.querySelector(".image-popup .close");
+var popup = document.querySelector(".image-popup");
+var images = document.querySelectorAll(".item-image img");
+var modalImg = document.getElementById("modalImg");
+var captionText = document.getElementById("caption");
+close.addEventListener("click", function(){
+  popup.setAttribute('style',"display: none;")
+});
+for (i in images) {
+    images[i].addEventListener("click", function(){
+    popup.setAttribute('style',"display: block;")
+    modalImg.src = this.src;
+    caption.innerHTML = this.alt;
+  });
+
+};
+
+
 
 $(function() {
   $(".snip-input").change(function(){
